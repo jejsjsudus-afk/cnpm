5835f7d387ecb3c461ea18bf726c21cd4fbbf2b8
Kien <jejsjsudus@gmail.com>
2026-04-09 18:05:31 +0700

    Fix SheetTrigger property for build

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,7 +18,7 @@ export default async function DashboardLayout({
 
   return (
     <div className="flex h-screen overflow-hidden bg-slate-50">
-      <Sidebar />
+      <Sidebar isMobile={false} />
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Header />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
